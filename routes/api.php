261d7b2bd23cee50a3a28c9ea2d2b60cb39fad03
fahrul31 <fahrulhidayat73@gmail.com>
2022-04-08 15:38:07 +0800

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\PaketWisataController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::prefix('admin')->group(function () {
+    Route::post('paket/create', [PaketWisataController::class, 'create']);
+    Route::get('paket', [PaketWisataController::class, 'allPaketWisata']);
+    Route::get('paket/{id}', [PaketWisataController::class, 'paketWisataByID']);
+    Route::put('paket/update/{id}', [PaketWisataController::class, 'update']);
+    Route::delete('paket/delete/{id}', [PaketWisataController::class, 'delete']);
+});
+
+
+Route::post('login',[AuthController::class,'login']);
+Route::post('user/create',[UserController::class,'create']);
+Route::group(['middleware' => 'auth:sanctum'],function(){
+    Route::get('user',[UserController::class,'allUser']);
+    Route::get('user/{id}',[UserController::class,'UserByID']);
+    Route::post('user/update/{id}',[UserController::class,'update']);
+    Route::delete('user/delete/{id}',[UserController::class,'delete']);
+});
+
